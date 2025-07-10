dfa15625439d080a509a5a5ac59afb29c00d70a2
Thabang Nkomo <thabang.nkomo@exazeit.com>
2025-07-10 16:18:41 +0200

    update

---
+++ builderPatternPractice/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builderPatternPractice
+{
+    public partial class Product
+    {
+        public string _name { get;private set; }
+        public Double _price { get;private set; }
+    }
+}
---
+++ builderPatternPractice/ProductBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace builderPatternPractice
+{
+    public partial class Product
+    {
+        public class ProductBuilder
+        {
+            private readonly Product _product = new Product();
+            public ProductBuilder SetName(string name)
+            {
+                this._product._name = name;
+                return this;
+            }
+
+            public ProductBuilder SetPrice(double price)
+            {
+                this._product._price = price;
+                return this;
+            }
+
+            public Product Build()
+            {
+                return _product;
+            }
+        }  
+    }
+}
